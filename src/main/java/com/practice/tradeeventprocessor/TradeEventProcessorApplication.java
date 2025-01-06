80f3729b0b9c6e13b780b0bcca376cbc231a70ca
TobyWang1 <fufummumm@gmail.com>
2025-01-06 20:47:49 +0000

    Refactor to enable Kafka and Scheduler; Change schedule rate

--- src/main/java/com/practice/tradeeventprocessor/TradeEventProcessorApplication.java
+++ src/main/java/com/practice/tradeeventprocessor/TradeEventProcessorApplication.java
@@ -2,10 +2,15 @@ package com.practice.tradeeventprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableKafka
+@EnableScheduling
 public class TradeEventProcessorApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(TradeEventProcessorApplication.class, args);
 	}
 }
--- src/main/java/com/practice/tradeeventprocessor/producer/TradeEventScheduler.java
+++ src/main/java/com/practice/tradeeventprocessor/producer/TradeEventScheduler.java
@@ -2,7 +2,9 @@ package com.practice.tradeeventprocessor.producer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TradeEventScheduler {
 
     private final TradeEventProducerService tradeEventProducerService;
@@ -12,7 +14,7 @@ public class TradeEventScheduler {
         this.tradeEventProducerService = tradeEventProducerService;
     }
 
-    @Scheduled(fixedRate = 5000) // Sends and event every 5 seconds
+    @Scheduled(fixedRate = 10000) // Sends and event every 10 seconds
     public void produce() {
         tradeEventProducerService.sendTradeEvents();
     }
